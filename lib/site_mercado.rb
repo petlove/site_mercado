5f948654446908615c2d02fe9e913e902797bd60
Ricardo Eihara <ricardo.hejr@gmail.com>
2019-12-16 20:25:29 -0300

    Init gem site_mercado

---
+++ lib/site_mercado.rb
@@ -0,0 +1,6 @@
+require "site_mercado/version"
+
+module SiteMercado
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/site_mercado_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe SiteMercado do
+  it "has a version number" do
+    expect(SiteMercado::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
