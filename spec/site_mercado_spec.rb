97a3c55f23736433eda0e195e20493288776daa6
Ricardo Eihara <ricardo.hejr@gmail.com>
2019-12-17 10:49:24 -0300

    Add rspec as dev dependency

--- spec/site_mercado_spec.rb
+++ spec/site_mercado_spec.rb
@@ -1,9 +1,5 @@
 RSpec.describe SiteMercado do
   it "has a version number" do
-    expect(SiteMercado::VERSION).not_to be nil
-  end
-
-  it "does something useful" do
-    expect(false).to eq(true)
+    expect(SiteMercado::VERSION).to eq('0.1.0')
   end
 end
