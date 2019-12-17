ae3b7d0c3f5c03025944ac10ea7f9d761fd1af28
Paulo Tada <paulohtfs@gmail.com>
2019-12-17 17:22:57 -0300

    Adds event and order models

---
+++ lib/site_mercado/models/v1/customer.rb
@@ -0,0 +1,19 @@
+module SiteMercado
+  class Customer
+    DICTIONARY = {
+      id: :id,
+      nome: :name,
+      email: :email,
+      cpf: :cpf,
+      rg: :rg,
+      tipo: :type,
+      publicidadeEmail: :public_email,
+      publicidadeSms: :public_sms,
+      dataNascimento: :birthday,
+      genero: :gender,
+      telefoneCelular: :phone_number,
+      telefoneFixo: :home_number,
+      enderecos: :address
+    }
+  end
+end
