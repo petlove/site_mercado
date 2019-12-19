edae7dac93089485ce1bd875dfaca5d399dc3495
Raul Fernando <raulfernando08@gmail.com>
2019-12-19 16:23:16 -0300

    Improve spec helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,23 +1,22 @@
 # frozen_string_literal: true
 
 require 'bundler/setup'
+require 'simplecov'
+require 'support/configs/simple_cov_config'
+require 'dotenv'
+
+SimpleCovConfig.configure
+
 require 'site_mercado'
 
 Dir[File.expand_path(File.join(File.dirname(__FILE__), 'support', '**', '*.rb'))].sort.each do |f|
   require f
 end
 
-SimpleCovConfig.configure
 VCRConfig.configure
-
-require 'dotenv'
-Dotenv.load('.env.test')
+Dotenv.load
 
 RSpec.configure do |config|
-  # Enable flags like --only-failures and --next-failure
-  config.example_status_persistence_file_path = '.rspec_status'
-
-  # Disable RSpec exposing methods globally on `Module` and `main`
   config.disable_monkey_patching!
 
   config.expect_with :rspec do |c|
