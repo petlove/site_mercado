fcb345329203231108a3b49c0fa7f09480d91baf
Anderson <andyferreira92@gmail.com>
2019-12-20 10:57:25 -0300

    Covers entities on spec

--- spec/lib/entities/address_spec.rb
+++ spec/lib/entities/address_spec.rb
@@ -1,4 +1,15 @@
 require 'spec_helper'
 
 RSpec.describe SiteMercado::Entities::Address do
+  it_behaves_like 'entity_attributes', %i[
+    id
+    logradouro
+    numero
+    complemento
+    bairro
+    cidade
+    uf
+    estado
+    cep
+  ]
 end
