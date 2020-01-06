00a6b3e3d4e6aef69da920220035c84727c1b926
Paulo Tada <paulohtfs@gmail.com>
2020-01-06 19:31:33 -0300

    Refacts default_url method

--- lib/site_mercado/configuration.rb
+++ lib/site_mercado/configuration.rb
@@ -4,12 +4,8 @@ module SiteMercado
   class Configuration
     attr_accessor :client_id, :client_secret, :endpoint, :api_version
 
-    def self.default_url
-      'https://service.sitemercado.com.br/'
-    end
-
     def initialize
-      @endpoint = ENV['SITEMERCADO_URL'] || self.class.default_url
+      @endpoint = ENV['SITEMERCADO_URL'] || 'https://service.sitemercado.com.br/'
       @api_version = ENV['SITEMERCADO_API_VERSION'] || '/api/v1/'
     end
   end
